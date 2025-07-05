d9d1f794a26f4d736920af4bfca93da157f8e54e
Zacky Mrf <zackymrf@gmail.com>
2025-07-05 23:49:04 +0700

    force redeploy: bypass cache to update title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+// Force redeploy - July 5, 2025
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
